6a9aa9f1447a815f97ebf058bff5358b343b7d48
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-25 12:56:03 +0900

    api urls.py作成

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
+from .views import ArticleViewSet, CategoryViewSet, CreateUserView, ListUserView, LoginUserView, ProfileViewSet
 
 router = routers.DefaultRouter()
+router.register('category', CategoryViewSet)
+router.register('articles', ArticleViewSet)
+router.register('profile', ProfileViewSet)
 
 urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
+    path('users/', ListUserView.as_view(), name='users'),
+    path('loginuser/', LoginUserView.as_view(), name='loginuser'),
     path('', include(router.urls)),
 ]
