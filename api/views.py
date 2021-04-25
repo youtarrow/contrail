32f8397cd3ead1335c8c25014fae6c9271bb61f9
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-25 12:40:59 +0900

    api views.py作成

--- api/views.py
+++ api/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from rest_framework import status, permissions, generics, viewsets
+from .serializers import UserSerializer, CategorySerializer, ArticleSerializer, ProfileSerializer
+from rest_framework.response import Response
+from .models import Profile, Category, Article
+from django.contrib.auth.models import User
+from . import custompermissions
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class ListUserView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class LoginUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'DELETEメソッドは許可されていません'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'PATCHメソッドは許可されていません'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'DELETEメソッドは許可されていません'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'PUTメソッドは許可されていません'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'PATCHメソッドは許可されていません'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticated, custompermissions.OwnerPermission,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'PATCHメソッドは許可されていません'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
