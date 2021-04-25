3e5e7b482300fa0144093f0470d21cd383c504c9
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-25 20:28:35 +0900

    counter削除

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/auth/authSlice";
 import articleReducer from "../features/article/articleSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    counter: counterReducer,
     article: articleReducer,
   },
 });
