6c032762efb80407cb9a6db2561b6ffa0626bcd5
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-26 20:58:21 +0900

    frontend : Article 詳細画面作成

--- frontend/src/features/article/ArticleDisplay.tsx
+++ frontend/src/features/article/ArticleDisplay.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectSelectedArticle } from "./articleSlice";
+import { Table, TableBody, TableCell, TableRow } from "@material-ui/core";
 
 const ArticleDisplay: React.FC = () => {
-  return <div></div>;
+  const selectedArticle = useSelector(selectSelectedArticle);
+  const rows = [
+    { item: "Title", data: selectedArticle.title },
+    { item: "Content", data: selectedArticle.content },
+    { item: "Owner", data: selectedArticle.owner_username },
+    { item: "Category", data: selectedArticle.category_item },
+    { item: "Created", data: selectedArticle.created_at },
+    { item: "Updated", data: selectedArticle.updated_at },
+  ];
+
+  if (!selectedArticle.title) {
+    return null;
+  }
+
+  return (
+    <>
+      <h2>Article details</h2>
+      <Table>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.item}>
+              <TableCell align="center">
+                <strong>{row.item}</strong>
+              </TableCell>
+              <TableCell align="center">{row.data}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
 };
 
 export default ArticleDisplay;
