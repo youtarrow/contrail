913e4ef565e2455234f8bf25307954b2cfe5a9bb
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-26 10:29:09 +0900

    frontend ArticleList更新

--- frontend/src/features/article/ArticleList.tsx
+++ frontend/src/features/article/ArticleList.tsx
@@ -1,6 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./ArticleList.module.css";
+
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchAsyncDeleteArticle,
+  selectArticles,
+  editArticle,
+  selectArticle,
+} from "./articleSlice";
+import { selectLoginUser, selectProfiles } from "../auth/authSlice";
+import { AppDispatch } from "../../app/store";
+import { initialState } from "./articleSlice";
+import { SORT_STATE, READ_ARTICLE } from "../types";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  table: {
+    tableLayout: "fixed",
+  },
+  button: {
+    margin: theme.spacing(3),
+  },
+  small: {
+    margin: "auto",
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+}));
 
 const ArticleList: React.FC = () => {
+  const classes = useStyles();
+  const dispatch: AppDispatch = useDispatch();
+  const articles = useSelector(selectArticles);
+  const loginUser = useSelector(selectLoginUser);
+  const profiles = useSelector(selectProfiles);
+  const columns = articles[0] && Object.keys(articles[0]);
+
+  const [state, setState] = useState<SORT_STATE>({
+    rows: articles,
+    order: "desc",
+    activeKey: "",
+  });
+
+  const handleClickSortColumn = (column: keyof READ_ARTICLE) => {
+    const isDesc = column === state.activeKey && state.order === "desc";
+    const newOrder = isDesc ? "asc" : "desc";
+    const sortedRows = Array.from(state.rows).sort((a, b) => {
+      if (a[column] > b[column]) {
+        return newOrder === "asc" ? 1 : -1;
+      } else if (a[column] < b[column]) {
+        return newOrder === "asc" ? -1 : 1;
+      } else {
+        return 0;
+      }
+    });
+
+    setState({
+      rows: sortedRows,
+      order: newOrder,
+      activeKey: column,
+    });
+  };
+
+  useEffect(() => {
+    setState((state) => ({
+      ...state,
+      rows: articles,
+    }));
+  }, [articles]);
+
+  const conditionalSrc = (user: number) => {
+    const loginProfile = profiles.filter(
+      (prof) => prof.user_profile === user
+    )[0];
+    return loginProfile?.img !== null ? loginProfile?.img : undefined;
+  };
+
   return <div></div>;
 };
 
